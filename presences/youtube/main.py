8dab6eb763fae670c5ebd7fa673f55dc4bf3a48b
Manuel Cabral <mcabral@frba.utn.edu.ar>
2023-06-01 00:29:40 -0300

    feat(youtube): checks

--- presences/youtube/main.py
+++ presences/youtube/main.py
@@ -1,7 +1,12 @@
 import time
 
 while running:
+    url = runtime.url
+    if not "youtube.com" in url:
+        continue
     media = runtime.mediaSession()
+    if media is None:
+        continue
     update_rpc(
         details="Viewing a video",
         state=media.title,
