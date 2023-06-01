f26d693dfea167ea92a6428ad23714de07896d75
Manuel Cabral <mcabral@frba.utn.edu.ar>
2023-06-01 00:37:16 -0300

    feat(youtube): add docstring

--- presences/youtube/main.py
+++ presences/youtube/main.py
@@ -1,10 +1,16 @@
 import time
 
+"""
+In this example, "runtime" is used to first retrieve the "mediaSession", which allows us to obtain information such as author, image, etc.
+Then, the URL is checked to see if it is from YouTube to continue updating the presence.
+Note this example updates every 2 seconds but the real presence updates every 15 seconds.
+"""
+
 while running:
+    media = runtime.mediaSession()
     url = runtime.url
     if not "youtube.com" in url:
         continue
-    media = runtime.mediaSession()
     if media is None:
         continue
     update_rpc(
