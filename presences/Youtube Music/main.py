af2c5c838db2471c75364a7cb2e7b6ff9bbb5423
Manu <mcabral@frba.utn.edu.ar>
2023-06-14 18:44:15 -0300

    feat(presences): add Youtube Music

---
+++ presences/Youtube Music/main.py
@@ -0,0 +1,64 @@
+import time
+
+
+def next():
+    time.sleep(10)
+
+
+while running:
+    tab = None
+    current_tab = runtime.current_tab
+    ytm_tabs = runtime.tabs(url_pattern="music.youtube.com")
+
+    if "music.youtube.com" in current_tab.url:
+        # Current tab is a YouTube Music tab
+        tab = current_tab
+    elif len(ytm_tabs) > 0:
+        # If the current tab is not a YouTube Music tab, but there are YouTube Music tabs open, use the first one
+        tab = ytm_tabs[0]
+
+    if tab is None:
+        # No YouTube Music tabs open
+        print("No tab found")
+        next()
+        continue
+
+    media = tab.media_session()
+
+    if media is None and "channel" in tab.url:
+        # YouTube Music tab is on a channel page (not playing anything)
+        update_rpc(
+            state="Browsing...",
+            buttons=[
+                {
+                    "label": "View Channel",
+                    "url": tab.url,
+                },
+            ],
+        )
+        next()
+        continue
+
+    # selector to get the artist url
+    artist_url = tab.execute(
+        'document.querySelector("span.subtitle.style-scope.ytmusic-player-bar > yt-formatted-string > a:nth-child(1)").href'
+    )
+    is_playing = media.state == "playing"
+    update_rpc(
+        state=media.title,
+        details=media.artist,
+        large_image=media.image,
+        small_image="playing" if is_playing else "pause",
+        small_text=media.state.capitalize(),
+        buttons=[
+            {
+                "label": "Listen on YouTube Music",
+                "url": tab.url,
+            },
+            {
+                "label": "View Artist",
+                "url": artist_url,
+            },
+        ],
+    )
+    next()
