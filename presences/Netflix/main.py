796d59ea84696d0a8a1b310b8174458cff34a9f5
Manu <mcabral@frba.utn.edu.ar>
2023-06-14 20:49:48 -0300

    refactor: update netflix
    add compatibility with Presencify v0.0.6

--- presences/Netflix/main.py
+++ presences/Netflix/main.py
@@ -1,5 +1,6 @@
 import time
 
+# Function to get the current video metadata
 videometadata_func = """
 function getVideoMetadata(){
     let appId = Object.keys(netflix.appContext.state.playerApp.getState().videoPlayer.videoMetadata)[0];
@@ -9,6 +10,7 @@ function getVideoMetadata(){
 }
 """
 
+# Function to get the current show metadata
 showmetadata_func = """
 function getShowMetadata(){
     let appId = Object.keys(netflix.appContext.state.playerApp.getState().videoPlayer.videoMetadata)[0];
@@ -30,34 +32,58 @@ function getShowMetadata(){
     return `${foundSeason.shortName} E${episodeNumber}: ${foundEpisode.title}`;
 }
 """
+
+# Not uses assets
 netflix_logo = "https://images.ctfassets.net/4cd45et68cgf/Rx83JoRDMkYNlMC9MKzcB/2b14d5a59fc3937afd3f03191e19502d/Netflix-Symbol.png"
-check_time = 5
+check_time = 10  # Time to check for updates (in seconds)
 
 while running:
-    if "netflix.com" not in runtime.url:
+    tab = runtime.current_tab
+
+    # Not on Netflix
+    if "netflix.com" not in tab.url:
         time.sleep(check_time)
         continue
-    if "browse" in runtime.url:
+
+    # Not watching anything, just browsing
+    if "browse" in tab.url:
         update_rpc(details="Browsing...", large_image=netflix_logo)
         time.sleep(check_time)
         continue
-    runtime.execute(videometadata_func)
-    video_metadata = runtime.execute("getVideoMetadata()")
+
+    # Watching something
+    tab.execute(videometadata_func)
+    video_metadata = tab.execute("getVideoMetadata()")
+
+    # Skip if no metadata
     if video_metadata == "":
         continue
+
+    # Extract each metadata
     video_type, video_title, video_image = video_metadata.split("#")
+    is_paused = tab.execute("document.querySelector('video').paused")
+
+    # If watching a show
     if video_type == "show":
-        runtime.execute(showmetadata_func)
-        info = runtime.execute(f"getShowMetadata()")
+        tab.execute(showmetadata_func)
+        info = tab.execute("getShowMetadata()")
         update_rpc(
             details=info,
             state=video_title,
             large_image=video_image,
+            small_image="pause" if is_paused else "play",
+            small_text="Paused" if is_paused else "Playing",
         )
+
+    # If watching a movie
     elif video_type == "movie":
         update_rpc(
             details=video_title,
             state="Watching a Movie",
             large_image=video_image,
+            small_image="pause" if is_paused else "play",
+            small_text="Paused" if is_paused else "Playing",
         )
+
+    # Wait for next check
     time.sleep(check_time)
