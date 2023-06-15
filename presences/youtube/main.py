436acb023f1f9a9f8bd65de80c216af68a56c2a3
Manu <mcabral@frba.utn.edu.ar>
2023-06-14 21:05:55 -0300

    feat(presences): update Youtube

--- presences/youtube/main.py
+++ presences/youtube/main.py
@@ -1,24 +1,32 @@
+"""
+This version only updates the presence when the tab is active.
+So if you have multiple Youtube tabs open, it will only update the active one.
+"""
 import time
 
 while running:
-    url = runtime.url
-    if not "www.youtube.com" in url:
+    tab = runtime.current_tab
+    if not "www.youtube.com" in tab.url:
+        time.sleep(10)
         continue
 
-    media = runtime.mediaSession()
+    media = tab.media_session()
     if not media:
+        time.sleep(10)
         continue
 
-    channel_url = runtime.execute("document.querySelector('#text > a').href")
-
+    channel_url = tab.execute("document.querySelector('#text > a').href")
+    paused = media.state == "paused"
     update_rpc(
         details=media.artist,
         state=media.title,
         large_image=media.image,
+        small_image="pause" if paused else "play",
+        small_text=media.state.capitalize(),
         buttons=[
-            {"label": "Play Video", "url": url},
+            {"label": "Play Video", "url": tab.url},
             {"label": "View Channel", "url": channel_url},
         ],
     )
 
-    time.sleep(5)
+    time.sleep(10)
